169503e88daaf40cf01384bf52f1d5efa1b34138
JuanOnate <19030179@itcelaya.edu.mx>
2023-09-06 07:08:59 -0600

    Carrusel añadido
    Código inicial del main.
    image_carousel.dart creado.
    Código completo de este nuevo archivo.
    Dependencia de carousel añadida a pubspec.yaml.
    Creación de directorio images para las imágenes.
    Directorio images dado de alta en pubspec.yaml.
    Imágenes agregadas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:practica2/image_carousel.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+  runApp(FirstScreen());
+} 
+class FirstScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return _FirstScreen();
   }
+} 
+class _FirstScreen extends State<FirstScreen> { 
+ @override 
+ Widget build(BuildContext context) {
+  return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.amber[200],
+        leading: Row(
+          children: <Widget>[
+            SizedBox(
+              width: 5.0,
+            ),
+            IconButton(
+              color: Colors.white,
+              icon: const Icon(
+                Icons.arrow_back,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+        ],
+      ),
+      backgroundColor: Colors.amber[200],
+      
+      body: ListView(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              CarouselWithIndicatorDemo(), 
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
 }
